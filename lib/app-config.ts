d82142e8a071621fdb11c25a5f49204d73843b36
Nicolás San Marco <148972807+Nicosm1988@users.noreply.github.com>
2025-08-25 17:07:45 -0300

    Update app-config.ts

--- lib/app-config.ts
+++ lib/app-config.ts
@@ -12,7 +12,7 @@ export const appConfig: {
     "name": "ai-deploy-one-click UI",
     "shortDescription": "An AI chatbot",
     "extendedDescription": "User interface for an AI chatbot",
-    "aiModel": "GPT 4.1",
+    "aiModel": "Gemini 2.5 Pro",
     "systemPrompt": "You are a helpful assistant.",
     "appUrl": "https://github.com/AstraBert/ai-deploy-one-click-ui",
     "gitHubSource": "AstraBert/ai-deploy-one-click-ui"
