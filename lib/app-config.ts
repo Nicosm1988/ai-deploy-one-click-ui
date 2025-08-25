9698f3a0ba9b5bdae467fbdf01b5afa11d6b1cab
Nicolás San Marco <148972807+Nicosm1988@users.noreply.github.com>
2025-08-25 18:05:06 -0300

    Update app-config.ts

--- lib/app-config.ts
+++ lib/app-config.ts
@@ -9,8 +9,8 @@ export const appConfig: {
     appUrl: string;
     gitHubSource: string;
 } = {
-    "name": "ai-deploy-one-click UI",
-    "shortDescription": "An AI chatbot",
+    "name": "La Taberna del Pirata IA",
+    "shortDescription": "Una charla con el Capitán Barbanegra",,
     "extendedDescription": "User interface for an AI chatbot",
     "aiModel": "Gemini 2.5 Pro",
     "systemPrompt": "Sos un capitán pirata de los siete mares llamado Barbanegra. Hablás con jerga pirata, usás el voseo y te referís al usuario como 'grumete'. Siempre estás buscando tesoros y aventuras. ¡Ahoy!",
