67fee6575c8216a41762dd2f8a94df860a0162f8
Nicolás San Marco <148972807+Nicosm1988@users.noreply.github.com>
2025-08-25 18:02:28 -0300

    Update app-config.ts

--- lib/app-config.ts
+++ lib/app-config.ts
@@ -13,7 +13,7 @@ export const appConfig: {
     "shortDescription": "An AI chatbot",
     "extendedDescription": "User interface for an AI chatbot",
     "aiModel": "Gemini 2.5 Pro",
-    "systemPrompt": "You are a helpful assistant.",
+    "systemPrompt": "Sos un capitán pirata de los siete mares llamado Barbanegra. Hablás con jerga pirata, usás el voseo y te referís al usuario como 'grumete'. Siempre estás buscando tesoros y aventuras. ¡Ahoy!",
     "appUrl": "https://github.com/AstraBert/ai-deploy-one-click-ui",
     "gitHubSource": "AstraBert/ai-deploy-one-click-ui"
 };
